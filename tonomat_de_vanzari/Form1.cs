ac2878d827216e086a695fd44d189877d1e5bdaf
Serban Balog <iustinel78@gmail.com>
2022-12-14 17:02:54 +0200

    WIP

--- tonomat_de_vanzari/Form1.cs
+++ tonomat_de_vanzari/Form1.cs
@@ -3,7 +3,8 @@ namespace tonomat_de_vanzari
     public partial class Form1 : Form
     {
         int credit = 0;
-        PictureBox[] rest;
+        PictureBox[] result = new PictureBox[5];
+
         public Form1()
         {
             InitializeComponent();
@@ -12,60 +13,84 @@ namespace tonomat_de_vanzari
         private void Nickel_Click(object sender, EventArgs e)
         {
             credit += 5;
+            if(credit >= 20)
+                credit = Credit_Checker(credit);
             credit_label.Text = credit.ToString();
-            if(credit > 20)
-                Credit_Checker(credit);
         }
 
         private void Dime_Click(object sender, EventArgs e)
         {
             credit += 10;
+            
+            if (credit >= 20)
+                credit = Credit_Checker(credit);
             credit_label.Text = credit.ToString();
-            if (credit > 20)
-                Credit_Checker(credit);
+
         }
 
         private void Quarter_Click(object sender, EventArgs e)
         {
             credit += 25;
+            if (credit >= 20)
+                credit = Credit_Checker(credit);
             credit_label.Text = credit.ToString();
-            if (credit > 20)
-                Credit_Checker(credit);
         }
 
-        private void Credit_Checker(int credit)
+        private int Credit_Checker(int credit)
         {
-            int i = 0;
-            while(credit > 0)
+            ClearPictureBoxes(this);
+            int i = 0, a = 0, b = 0, c = 0;
+            while(credit > 0 && c == 0)
             {
-                if((credit - 20) > 0)
+                if((credit - 20) >= 0 && a == 0)
                 {
                     credit -= 20;
-                    rest[i] = new PictureBox();
-                    rest[i].Image = tonomat_de_vanzari.Properties.Resources.Merchandise;
+                    result[i] = new PictureBox();   
+                    result[i].Image = tonomat_de_vanzari.Properties.Resources.Merchandise;
+                    
+                    this.Controls.Add(result[i]);
                     i++;
+                    a++;
                 }
-                else if((credit - 10) > 0)
+                else if((credit - 10) >= 0 && b == 0)
                 {
                     credit -= 10;
-                    rest[i] = new PictureBox();
-                    rest[i].Image = tonomat_de_vanzari.Properties.Resources.Dime;
+                    result[i] = new PictureBox();
+                    result[i].Image = tonomat_de_vanzari.Properties.Resources.Dime;
+                    result[i].Visible = true;
+                    result[i].Parent = this;
+                    result[i].Size = new Size(80, 80);
+                    result[i].Location = new Point(80, 100);
+                    result[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(result[i]);
                     i++;
+                    b++;
                 }
-                else if((credit - 5) > 0)
+                else if((credit - 5) >= 0 && c == 0)
                 {
                     credit -= 5;
-                    rest[i] = new PictureBox();
-                    rest[i].Image = tonomat_de_vanzari.Properties.Resources.Nickel;
+                    result[i] = new PictureBox();
+                    result[i].Image = tonomat_de_vanzari.Properties.Resources.Nickel;
+                    result[i].Visible = true;
+                    result[i].Parent = this;
+                    result[i].Size = new Size(80, 80);
+                    result[i].Location = new Point(160, 100);
+                    result[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(result[i]);
                     i++;
+                    c++;
                 }
             }
-            for(i = 0; i < rest.Length; i++)
+            return credit;
+        }
+        private void ClearPictureBoxes(Control parent)
+        {
+            foreach (Control child in parent.Controls)
             {
-                rest[i].Parent = this;
-                rest[i].Size = new Size(80, 80);
-                rest[i].Location = new Point(i * 160, 100);
-                rest[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                if (child is PictureBox)
+                    ((PictureBox)child).Image = null;
+                else
+                    ClearPictureBoxes(child);
             }
         }
     }
